69103f8dc4b548930470023a5c8d6be5955df9db
michzio <michzio@hotmail.com>
2016-09-25 16:35:09 +0200

    strings and base64 functions

---
+++ base64.h
@@ -0,0 +1,17 @@
+//
+// Created by Michal Ziobro on 24/09/2016.
+//
+
+#ifndef COMMON_FUNCTIONS_BASE64_H
+#define COMMON_FUNCTIONS_BASE64_H
+
+#include <ntsid.h>
+
+char *base64_encode(const unsigned char *data, const size_t data_len, size_t *base64_len);
+unsigned char *base64_decode(const char *base64, const size_t base64_len, size_t *data_len);
+// alternative implementation of base64 encoder and decoder
+int base64encode(const void* data_buf, size_t dataLength, char* result, size_t resultSize);
+int base64decode (char *in, size_t inLen, unsigned char *out, size_t *outLen);
+
+
+#endif //COMMON_FUNCTIONS_BASE64_H
